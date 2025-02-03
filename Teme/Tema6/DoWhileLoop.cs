fda3c15b4431ed5ad148aa33a08b22a7a30ae70d
Sorin Carpinisanu <43853369+SCarpinisanu@users.noreply.github.com>
2025-02-03 12:35:10 +0200

    Add files via upload
    Adding Tema6 - Loops

---
+++ Teme/Tema6/DoWhileLoop.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6
+{
+    static class DoWhileLoop
+    {
+
+        [Test]
+        public static void DoWhileMethod()
+        {
+            CountSkipFinish(200);
+        }
+
+        private static void CountSkipFinish(int finishValue)
+        {
+            int counter = 0;
+            do
+            {
+                counter++;
+                if (counter != 10)
+                {
+                    Console.Write($"{counter}; ");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine($"Number skipped!");
+                    do
+                    {
+                        counter++;
+                        Console.Write($"{counter}; ");
+                        //counter++;
+                        if (counter == 98 )
+                        {
+                            counter++;
+                            break;
+                        }
+                    }
+                    while (counter < finishValue);
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine($"Cannot count past {counter}!");
+                Console.WriteLine($"The limit for the count was {finishValue}");
+                break;
+            }
+            while (counter < finishValue);
+        }
+    }
+}
---
+++ Teme/Tema6/WhileLoop.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6
+{
+    static class LoopsTraining
+    {
+
+        [Test]
+        public static void WhileMethod()
+        {
+            //We have the start and limit: 1 to 99 and skip value 10
+            int counter = 0;
+            int limitValue = 99;
+            while (counter <= limitValue)
+            {
+                counter++;
+                if (counter != 10)
+                {
+                    Console.Write($"{counter}; ");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Number skipped!");
+                    while (counter < limitValue)
+                    {
+                        counter++;
+                        Console.Write($"{counter}; ");
+                        if (counter == limitValue - 1)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Cannot count past 99!");
+                            break;
+                        }
+                     }
+                    break;
+                }
+            }
+        }
+    }
+}
